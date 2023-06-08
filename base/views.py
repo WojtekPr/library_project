63344fab7fb8eb6ce2bdcf05c0293f443e16cc28
WojtekPr <wojtekpr4545@gmail.com>
2023-06-08 13:01:28 +0200

    register&login page

--- base/views.py
+++ base/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def tasklist(request):
     return HttpResponse("TEST")
+
+def registerPage(request):
+    context = {}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
