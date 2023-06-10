8e4868f14bb592069e8e033e75888a09774c51b2
WojtekPr <wojtekpr4545@gmail.com>
2023-06-10 20:22:24 +0200

    10.06.23 update

--- library_project/settings.py
+++ library_project/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
 
 # Application definition
 
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
