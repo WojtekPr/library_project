655f2e6d3b34887e39d536762b136a03ac2ae9c7
WojtekPr <wojtekpr4545@gmail.com>
2023-06-10 23:33:08 +0200

    logn and register update

--- base/views.py
+++ base/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def HomePage(request):
-    context  = {}
-    return render(request, 'accounts/home.html', context)
+    return render(request, 'accounts/home.html')
 
 def registerPage(request):
-    context = {}
-    return render(request, 'accounts/register.html', context)
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        print(uname,email,pass1,pass2)
+    return render(request, 'accounts/register.html')
 
 def loginPage(request):
-    context = {}
-    return render(request, 'accounts/login.html', context)
+    return render(request, 'accounts/login.html')
