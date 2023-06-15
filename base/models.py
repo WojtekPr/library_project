adcbe79df3b054c95a54e17f69423561e711b9ff
WojtekPr <wojtekpr4545@gmail.com>
2023-06-15 19:20:57 +0200

    15.06 update

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    authors = models.CharField(max_length=255)
+    image = models.URLField()
+    description = models.TextField(null=True)
+    language = models.CharField(max_length=255, null=True)
+    category = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.title
--- base/urls.py
+++ base/urls.py
@@ -5,9 +5,12 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
-    path('register/', views.registerPage, name='register'), 
+    path('', views.registerPage, name='register'), 
     path('registered/', views.registeredPage, name='registered'), 
     path('home/', views.HomePage, name='home'),
     path('logout/', views.logoutPage, name='logout'),
+    path('save_book/', views.save_book, name='save_book'),
+    path('remove_book/<int:book_id>/', views.remove_book, name='remove_book'),
+    path('books/', views.book_list, name='book_list'),
 
 ]
